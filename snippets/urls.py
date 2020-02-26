dcf0a78997871cbc3ebaaed320be030189cf5081
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-25 20:33:16 -0800

    Update urls.py

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view())
+    path('users/<int:pk>/', views.UserDetail.as_view(),
+    path('', views.api_root),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
